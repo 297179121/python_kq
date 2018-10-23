0c8c0d7d6ae7fc96ce78fcf60d6da5f0eedf1eec
297179121 <297179121@qq.com>
2018-10-23 17:39:15 +0800

    first commit

--- cal/demo/excel.py
+++ cal/demo/excel.py
@@ -1,11 +1,11 @@
 from openpyxl import load_workbook
 import pymysql
 import time
-from datetime import datetime,timedelta, date
+import datetime
 import requests
 
-TIME_ON = "07:50"
-TIME_OFF = "16:35"
+TIME_ON_STRING = "07:50"
+TIME_OFF_STRING = "16:35"
 
 
 def insert_database(list_line):
@@ -89,23 +89,90 @@ def get_connect():
     return pymysql.connect("localhost", "root", "root", "attendance")
 
 
-def get_overtime_in_holiday(month):
+def get_overtime_in_workday(month):
     connect = get_connect()
     with connect.cursor() as cursor:
-        sql = " select att_date, att_starttime, att_endtime from bt_att_t where att_starttime is not null and att_endtime is not null and sign_holiday<>0 and att_date like concat('%%', %s,'%%')"
-        cursor.executemany(sql, (month))
+        month = "%" + month + "%"
+        sql = """
+        select att_date, att_starttime, att_endtime, sign_holiday from bt_att_t where att_date like %s order by att_date desc 
+        """
+        cursor.executemany(sql, [month])
+        result = cursor.fetchall()
+        close_connect(connect)
+        list_dict = []
+        for line in result:
+            date_str = line[0]
+            start_str = line[1]
+            end_str = line[2]
+            holiday_int = line[3]
+            # 节假日加班
+            if holiday_int != 0 and not start_str and not end_str:
+                date_datetime = datetime.datetime.strptime(date_str, "%Y-%m-%d")
+                time_date = date_datetime.date()
+
+                start_datetime = datetime.datetime.strptime(start_str, '%H:%M')
+                end_datetime = datetime.datetime.strptime(end_str, '%H:%M')
+                start_datetime = datetime.datetime.combine(time_date, start_datetime.time())
+                end_datetime = datetime.datetime.combine(time_date, end_datetime.time())
+
+                hours = round((end_datetime-start_datetime).seconds/3600, 1)
+                title = "节假日加班%s小时" % str(hours)
+                json = {"title": title, "start": line[0], "end": line[0]}
+                list_dict.append(json)
+            # 工作日加班
+            elif holiday_int == 0 and not start_str and not end_str:
+                date_datetime = datetime.datetime.strptime(date_str, "%Y-%m-%d")
+                time_date = date_datetime.date()
+                start_datetime = datetime.datetime.strptime(start_str, '%H:%M')
+                end_datetime = datetime.datetime.strptime(end_str, '%H:%M')
+                on_time = datetime.datetime.strptime(TIME_ON_STRING, '%H:%M')
+                off_time = datetime.datetime.strptime(TIME_OFF_STRING, '%H:%M')
+
+                start_datetime = datetime.datetime.combine(time_date, start_datetime.time())
+                end_datetime = datetime.datetime.combine(time_date, end_datetime.time())
+                on_time = datetime.datetime.combine(time_date, on_time.time())
+                off_time = datetime.datetime.combine(time_date, off_time.time())
+                if (on_time - start_datetime).seconds > 1800 and (end_datetime - off_time).seconds > 1800 :
+                    result
+
+
+def get_overtime_in_holiday(month):
+    """
+    节假日加班
+    :param month:
+    :return:
+    """
+    connect = get_connect()
+    with connect.cursor() as cursor:
+        month = "%" + month + "%"
+        sql = """select att_date, att_starttime, att_endtime 
+                  from bt_att_t 
+                  where att_starttime is not null and 
+                  att_endtime is not null and 
+                  sign_holiday<>0 and 
+                  att_date like %s 
+                  order by att_date desc"""
+
+        cursor.executemany(sql, [month])
         result = cursor.fetchall()
     close_connect(connect)
+    list_dict = []
     for line in result:
-        date_datetime = datetime.strptime(line[0], "%Y-%m-%d")
+        date_datetime = datetime.datetime.strptime(line[0], "%Y-%m-%d")
         time_date = date_datetime.date()
 
-        start_datetime = datetime.strptime(line[1], '%H:%M')
-        end_datetime = datetime.strptime(line[2], '%H:%M')
+        start_datetime = datetime.datetime.strptime(line[1], '%H:%M')
+        end_datetime = datetime.datetime.strptime(line[2], '%H:%M')
+        start_datetime = datetime.datetime.combine(time_date, start_datetime.time())
+        end_datetime = datetime.datetime.combine(time_date, end_datetime.time())
 
-        start_datetime = datetime.combine(time_date, start_datetime.time())
-        end_datetime = datetime.combine(time_date, end_datetime.time())
-    return result
+        hours = round((end_datetime-start_datetime).seconds/3600, 1)
+        title = "节假日加班%s小时" % str(hours)
+        json = {"title": title, "start": line[0], "end": line[0]}
+        list_dict.append(json)
+        print()
+
+    return list_dict
 
 
 def get_calendar():
@@ -119,6 +186,6 @@ def get_calendar():
         result = cursor.fetchall()
         print(result)
 
-read_excel('C:\\Users\\yhr\\Desktop\\考勤.xlsx')
+# read_excel('C:\\Users\\yhr\\Desktop\\考勤20181019.xlsx')
 
 
